48fc3ac92a715d28b08f673e19d2f59f292d1847
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-02-18 16:48:36 +0100

    Add Preconditions class

---
+++ src/ch/epfl/rigel/Preconditions.java
@@ -0,0 +1,10 @@
+package ch.epfl.rigel;
+
+/**
+ * Cette classe vérifie que les préconditions des arguments passés aux fonctions sont valides.
+ *
+ * @author Thomas Bonnardel (319827)
+ */
+public final class Preconditions {
+
+}
