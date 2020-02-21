1e37e420c94537820ea8ed607f81dfe1fb7dada2
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-02-21 14:03:53 +0100

    Implement SphericalCoordinates class

---
+++ src/ch/epfl/rigel/coordinates/SphericalCoordinates.java
@@ -0,0 +1,80 @@
+package ch.epfl.rigel.coordinates;
+
+import ch.epfl.rigel.math.Angle;
+
+import java.util.Objects;
+
+/**
+ * Classe mère à toutes les classes représentant des coordonnées sphériques.
+ *
+ * @author Thomas Bonnardel (319827)
+ */
+abstract class SphericalCoordinates {
+
+    private double lon;
+    private double lat;
+
+    SphericalCoordinates(double lon, double lat) {
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    /**
+     * Méthode qui retourne la longitude en radians.
+     *
+     * @return la longitude en radians.
+     */
+    double lon() {
+        return lon;
+    }
+
+    /**
+     * Méthode qui retourne la longitude en degrés.
+     *
+     * @return la longitude en degrés
+     */
+    double lonDeg() {
+        return Angle.toDeg(lon);
+    }
+
+    /**
+     * Méthode qui retourne la latitude en radians.
+     *
+     * @return la latitude en radians
+     */
+    double lat() {
+        return lat;
+    }
+
+    /**
+     * Méthode qui retourne la latitude en degrés.
+     *
+     * @return la latitude en degrés
+     */
+    double latDeg() {
+        return Angle.toDeg(lat);
+    }
+
+    /**
+     * Redéfinition de equal de Object en levant l'exception UnsupportedOperationException.
+     *
+     * @param o l'objet à tester
+     * @return la valeur de l'égalité des deux objets (jamais retourné)
+     * @throws UnsupportedOperationException dans tous les cas
+     */
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Redéfinition de hashCode de Object en levant l'exception UnsupportedOperationException.
+     *
+     * @return le hashCode (jamais retourné)
+     * @throws UnsupportedOperationException dans tous les cas
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(lon, lat);
+    }
+}
--- test/ch/epfl/rigel/PreconditionsTest.java
+++ test/ch/epfl/rigel/PreconditionsTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * @author Thomas Bonnardel (319827)
- */
 public final class PreconditionsTest {
 
     @Test
