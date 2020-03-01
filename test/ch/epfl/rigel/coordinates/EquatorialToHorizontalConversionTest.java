503140cefeb334ca207dc3b413b830072f3a85df
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-03-01 15:16:56 +0100

    Add and implement EquatorialToHorizontalConversion class

---
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
@@ -0,0 +1,11 @@
+package ch.epfl.rigel.coordinates;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Thomas Bonnardel (319827)
+ */
+public class EquatorialToHorizontalConversionTest {
+}
