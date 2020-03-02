c569b32d030b5dc2e80667753460dc99380b1a3b
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-03-02 13:41:29 +0100

    Add a test in EclipticToEquatorialConversionTest

--- test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.coordinates;
 
+import ch.epfl.rigel.math.Angle;
 import org.junit.jupiter.api.Test;
 
 import java.time.*;
@@ -11,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class EclipticToEquatorialConversionTest {
 
+    private final static double DELTA = 1e-4;
+
     @Test
     void EclipticToEquatorialConversionTestWorksWithValidParameters() {
         EclipticToEquatorialConversion c = new EclipticToEquatorialConversion(ZonedDateTime.of(
@@ -18,5 +21,29 @@ public class EclipticToEquatorialConversionTest {
            LocalTime.of(11, 45, 19),
            ZoneOffset.UTC
         ));
+    }
+
+    @Test
+    void conversionWorksWithTheExampleOfTheBook() {
+        // Page 54
+        EclipticCoordinates ecl = EclipticCoordinates.of(
+                Angle.ofDMS(139, 41, 10.),
+                Angle.ofDMS(4, 52, 31.)
+        );
+        ZonedDateTime d = ZonedDateTime.of(
+                LocalDate.of(2009, Month.JULY, 6),
+                LocalTime.of(20, 0),    // N'importe pas
+                ZoneOffset.UTC
+        );
+        EquatorialCoordinates equExpected = EquatorialCoordinates.of(
+                2.508430994,
+                0.340962273
+        );
+
+        EclipticToEquatorialConversion conversionObj = new EclipticToEquatorialConversion(d);
+        EquatorialCoordinates equ = conversionObj.apply(ecl);
+
+        assertEquals(equExpected.ra(), equ.ra(), DELTA);
+        assertEquals(equExpected.dec(), equ.dec(), DELTA);
     }
 }
