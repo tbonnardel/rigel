02f408ca7fa1e4e0f8e8a2a432e60bfe1ba8eaa6
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-03-13 13:55:33 +0100

    Add CelestialObjectModel interface

---
+++ src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
@@ -0,0 +1,24 @@
+package ch.epfl.rigel.astronomy;
+
+import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
+
+/**
+ * Cette classe représente un modèle d'objet céleste, c'est-à-dire d'une manière
+ * de calculer les caractéristiques de cet objet à un instant donné.
+ *
+ * @author Thomas Bonnardel (319827)
+ */
+public interface CelestialObjectModel<O> {
+
+    /**
+     * Méthode qui retourne l'objet modélisé par le modèle pour le nombre (éventuellement négatif)
+     * de jours après l'époque J2010 donné, en utilisant la conversion donnée pour obtenir
+     * ses coordonnées équatoriales à partir de ses coordonnées écliptiques.
+     *
+     * @param daysSinceJ2010 nombre de jours après l'époque J2000
+     * @param eclipticToEquatorialConversion la conversion pour obtenir ses coordonnées équatoriales
+     *                                       à partir de ses coordonnées écliptiques
+     * @return l'objet modélisé par le modèle pour les paramètres donnés
+     */
+    public abstract O at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion);
+}
