dafa96ec84d6be60c912c3de71cc495a641e13da
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-04-06 11:34:13 +0200

    Remove TODO comments

--- src/ch/epfl/rigel/astronomy/StarCatalogue.java
+++ src/ch/epfl/rigel/astronomy/StarCatalogue.java
@@ -72,7 +72,7 @@ public final class StarCatalogue {
             throw new IllegalArgumentException();
         }
 
-        return Collections.unmodifiableList(catalogue.get(asterism)); // TODO: Est-ce correct ?
+        return Collections.unmodifiableList(catalogue.get(asterism));
     }
 
     /**
