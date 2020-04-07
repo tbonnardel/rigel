c27c320c06378ab245cd59131c859e36f173b7d8
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-04-07 11:38:25 +0200

    Add BlackBodyColor class and ressources

---
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -0,0 +1,10 @@
+package ch.epfl.rigel.gui;
+
+/**
+ * Cette classe offre une méthode permettant d'obtenir la couleur
+ * d'un corps noir étant donnée sa température.
+ *
+ * @author Thomas Bonnardel (319827)
+ */
+public final class BlackBodyColor {
+}
