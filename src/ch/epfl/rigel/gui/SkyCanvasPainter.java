6bf08d976d92e73ea04cd8f5af8f0d369b997510
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-04-11 19:03:24 +0200

    WIP on SkyCanvasPainter: implement size methods

--- src/ch/epfl/rigel/gui/SkyCanvasPainter.java
+++ src/ch/epfl/rigel/gui/SkyCanvasPainter.java
@@ -1,11 +1,15 @@
 package ch.epfl.rigel.gui;
 
-import ch.epfl.rigel.astronomy.ObservedSky;
+import ch.epfl.rigel.astronomy.*;
 import ch.epfl.rigel.coordinates.StereographicProjection;
+import ch.epfl.rigel.math.Angle;
+import ch.epfl.rigel.math.ClosedInterval;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.transform.Transform;
+
+import static java.lang.Math.tan;
 
 /**
  * Cette classe instanciable représente un « peintre de ciel »,
@@ -98,5 +102,79 @@ public class SkyCanvasPainter {
 
     public void drawHorizon() {
         // TODO A implémenter
+    }
+
+
+    /**
+     * Méthode qui retourne le diamètre du disque représentant
+     * le Soleil dans notre projection.
+     *
+     * @param sun le Soleil à représenter
+     * @return le diamètre du disque représentant le Soleil
+     */
+    private double size(Sun sun) {
+        return effectiveSize(sun.angularSize());
+    }
+
+    /**
+     * Méthode qui retourne le diamètre du disque représentant
+     * la Lune dans notre projection.
+     *
+     * @param moon la Lune à représenter
+     * @return le diamètre du disque représentant la Lune
+     */
+    private double size(Moon moon) {
+        return effectiveSize(moon.angularSize());
+    }
+
+    /**
+     * Méthode qui retourne le diamètre du disque représentant
+     * la planète spécifiée dans notre projection.
+     *
+     * @param planet la planète à représenter
+     * @return le diamètre du disque représentant la planète
+     */
+    private double size(Planet planet) {
+        return basedOnMagnitudeSize(planet.magnitude());
+    }
+
+    /**
+     * Méthode qui retourne le diamètre du disque représentant
+     * l'étoile spécifiée dans notre projection.
+     *
+     * @param star l'étoile à représenter
+     * @return le diamètre du disque représentant l'étoile
+     */
+    private double size(Star star) {
+        return basedOnMagnitudeSize(star.magnitude());
+    }
+
+
+    /**
+     * Méthode privée qui calcule la taille effective de l'objet celeste
+     * projeté dans notre projection stéréographique en fonction de sa
+     * taille angulaire.
+     * Notez que cette formule n'est valable que pour le Soleil et la Lune.
+     *
+     * @param angularSize la taille angulaire
+     * @return la taille effective
+     */
+    private double effectiveSize(double angularSize) {
+        return 2*tan(angularSize/4);
+    }
+
+    /**
+     * Méthode privée qui calcule la taille du disque de l' objet céleste
+     * projeté dans notre projection stéréographique en fonction de sa magnitude.
+     * Notez que cette méthode de calcul n'est valable que pour les planètes
+     * et les étoiles (hors Soleil).
+     *
+     * @param magnitude la magnitude de l'objet céleste
+     * @return la taille de l'objet céleste
+     */
+    private double basedOnMagnitudeSize(double magnitude) {
+        double clipedMagnitude = ClosedInterval.of(-2, 5).clip(magnitude);
+        double f = (99 - 17*clipedMagnitude) / 140;
+        return f*effectiveSize(Angle.ofDeg(.5));
     }
 }
