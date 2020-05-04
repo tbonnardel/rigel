828160ec4860a19b4a8f6e200bc4cddd53785920
Thomas Bonnardel <tthomasbonnardel@gmail.com>
2020-05-04 19:13:40 +0200

    WIP on Main class

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.astronomy.AsterismLoader;
 import ch.epfl.rigel.astronomy.HygDatabaseLoader;
 import ch.epfl.rigel.astronomy.StarCatalogue;
 import ch.epfl.rigel.coordinates.GeographicCoordinates;
@@ -86,6 +87,7 @@ public class Main extends Application {
         dateTimeBean.setZonedDateTime(DEFAULT_ZONED_DATE_TIME);
         viewingParametersBean.setCenter(DEFAULT_VIEWING_CENTER);
         viewingParametersBean.setFieldOfViewDeg(DEFAULT_DEG_FIELD_OF_VIEW);
+        timeAnimator.setAccelerator(NamedTimeAccelerator.TIMES_300.getAccelerator());
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
@@ -95,86 +97,103 @@ public class Main extends Application {
      * Méthode privée qui créée et retourne la barre de contrôle.
      *
      * @return le panneau horizontal représentant la barre de contrôle
+     * @throws IOException en cas d'erreur entrée / sortie
      */
-    private HBox createControlBar() {
-        Label lonLabel = new Label("Longitude (°) :");
-        TextField lonTextField = new TextField();
-        TextFormatter<Number> lonTextFormatter = createTextFormatterForLon();
-        lonTextFormatter.valueProperty().addListener(
-                (p, o, n) -> observerLocationBean.setLonDeg(n.doubleValue())); // TODO A vérifier
-        lonTextField.setTextFormatter(lonTextFormatter);
-        lonTextField.setStyle(FIELD_STYLE);
-
-        Label latLabel = new Label("Latitude (°) :");
-        TextField latTextField = new TextField();
-        TextFormatter<Number> latTextFormatter = createTextFormatterForLat();
-        latTextFormatter.valueProperty().addListener(
-                (p, o, n) -> observerLocationBean.setLatDeg(n.doubleValue())); // TODO: A vérifier
-        latTextField.setTextFormatter(latTextFormatter);
-        latTextField.setStyle(FIELD_STYLE);
-
-        HBox observedLocationHBox = new HBox(
-                lonLabel, lonTextField, latLabel, latTextField);
-        observedLocationHBox.setStyle(CONTROL_BAR_HBOX_CHILDREN_STYLE);
-
-
-        Label dateLabel = new Label("Date :");
-        DatePicker datePicker = new DatePicker();
-        datePicker.valueProperty().addListener(
-                (p, o, n) -> dateTimeBean.setDate(n)); // TODO: A vérifier
-        datePicker.setStyle("-fx-pref-width: 120;");
-
-        Label timeLabel = new Label("Heure :");
-        TextField timeTextField = new TextField();
-        TextFormatter<LocalTime> timeTextFormatter = createLocalTimeFormatter();
-        timeTextFormatter.valueProperty().addListener(
-                (p, o, n) -> dateTimeBean.setTime(n)); // TODO: A vérifier
-        timeTextField.setTextFormatter(timeTextFormatter);
-        timeTextField.setStyle("-fx-pref-width: 75; -fx-alignment: baseline-right;");
-
-        ComboBox timeZoneComboBox = new ComboBox();
-        timeZoneComboBox.setStyle("-fx-pref-width: 180;");
-        timeZoneComboBox.setItems(FXCollections.observableArrayList(
-                ZoneId.getAvailableZoneIds()).sorted());
-        timeZoneComboBox.valueProperty().addListener(
-                (p, o, n) -> dateTimeBean.setZone(ZoneId.of(n.toString()))); // TODO: A vérifier
-
-        // TODO: Désactiver la saisie de données quand une animation est en cours
-
-        HBox observedDateTimeHBox = new HBox(dateLabel, datePicker, timeLabel, timeTextField, timeZoneComboBox);
-        observedDateTimeHBox.setStyle(CONTROL_BAR_HBOX_CHILDREN_STYLE);
-
-
-        ChoiceBox acceleratorChoiceBox = new ChoiceBox();
-        acceleratorChoiceBox.setItems(FXCollections.observableList(NamedTimeAccelerator.ALL));
-        //acceleratorChoiceBox.valueProperty().bind(Bindings.select(timeAnimator.acceleratorProperty(), "name")); // TODO: A vérifier
-        // timeAnimator.set // TODO: A vérifier
-
-        // TODO: Améliorer la lisibilité
-        // TODO: fermer le flot correctement
-        InputStream fontStream = getClass()
-                .getResourceAsStream("/Font Awesome 5 Free-Solid-900.otf");
-        Font fontAwesome = Font.loadFont(fontStream, 15);
-
-        Button resetButton = new Button(UNDO_ICON);
-        resetButton.setFont(fontAwesome);
-        // TODO: lier le bouton au bean associé
-        Button playPauseButton = new Button(PLAY_ICON);
-        playPauseButton.setFont(fontAwesome);
-        // TODO: lier le bouton au bean associé
-        // TODO: changer l'icone si nécessaire
-
-        HBox timeAnimationHBox = new HBox(acceleratorChoiceBox, resetButton, playPauseButton);
-        timeAnimationHBox.setStyle("-fx-spacing: inherit;");
+    private HBox createControlBar() throws IOException {
+        HBox observedLocationHBox = createObservedLocationHBox();
+        HBox observedDateTimeHBox = createObservedDateTimeHBox();
+        HBox timeAnimationHBox = createTimeAnimationHBox();
 
         HBox controlBar = new HBox(
                 observedLocationHBox, new Separator(Orientation.VERTICAL),
                 observedDateTimeHBox, new Separator(Orientation.VERTICAL),
                 timeAnimationHBox);
         controlBar.setStyle("-fx-spacing: 4; -fx-padding: 4;");
+
         return controlBar;
     }
 
+
+    /**
+     * Méthode privée qui créé et retourne le panneau de
+     * la position d'observation.
+     *
+     * @return le panneau de la position d'observation
+     */
+    private HBox createObservedLocationHBox() {
+        Label lonLabel = new Label("Longitude (°) :");
+        TextField lonTextField = new TextField();
+        TextFormatter<Number> lonTextFormatter = createTextFormatterForLon();
+        lonTextFormatter.valueProperty().addListener(
+                (p, o, n) -> observerLocationBean.setLonDeg(n.doubleValue())); // TODO: A vérifier / Faire un lien bidirectionnel ?
+        lonTextField.setTextFormatter(lonTextFormatter);
+        lonTextField.setStyle(FIELD_STYLE);
+        lonTextField.textProperty().bind(observerLocationBean.lonDegProperty().asString()); // TODO: Modifier format pour afficher les décimales (cf au démarrage)
+
+        Label latLabel = new Label("Latitude (°) :");
+        TextField latTextField = new TextField();
+        TextFormatter<Number> latTextFormatter = createTextFormatterForLat();
+        latTextFormatter.valueProperty().addListener(
+                (p, o, n) -> observerLocationBean.setLatDeg(n.doubleValue())); // TODO: A vérifier / Faire un lien bidirectionnel ?
+        latTextField.setTextFormatter(latTextFormatter);
+        latTextField.setStyle(FIELD_STYLE);
+        latTextField.textProperty().bind(observerLocationBean.latDegProperty().asString()); // TODO: Modifier format pour afficher les décimales
+
+        HBox observedLocationHBox = new HBox(
+                lonLabel, lonTextField, latLabel, latTextField);
+        observedLocationHBox.setStyle(CONTROL_BAR_HBOX_CHILDREN_STYLE);
+
+        return observedLocationHBox;
+    }
+
+    /**
+     * Méthode privée qui créé et retourne le panneau de
+     * l'instant d'observation.
+     *
+     * @return le panneau de l'instant d'observation
+     */
+    private HBox createObservedDateTimeHBox() {
+        Label dateLabel = new Label("Date :");
+        DatePicker datePicker = new DatePicker();
+        datePicker.valueProperty().addListener(
+                (p, o, n) -> dateTimeBean.setDate(n)); // TODO: A vérifier / Faire un lien bidirectionnel ?
+        datePicker.setStyle("-fx-pref-width: 120;");
+        datePicker.valueProperty().bind(dateTimeBean.dateProperty());
+
+        Label timeLabel = new Label("Heure :");
+        TextField timeTextField = new TextField();
+        TextFormatter<LocalTime> timeTextFormatter = createLocalTimeFormatter();
+        timeTextFormatter.valueProperty().addListener(
+                (p, o, n) -> dateTimeBean.setTime(n)); // TODO: A vérifier / Faire un lien bidirectionnel ?
+        timeTextField.setTextFormatter(timeTextFormatter);
+        timeTextField.setStyle("-fx-pref-width: 75; -fx-alignment: baseline-right;");
+        dateTimeBean.timeProperty().addListener(
+                (p, o, n) -> timeTextField.setText(
+                        String.format("%02d:%02d:%02d",
+                                dateTimeBean.getTime().getHour(),
+                                dateTimeBean.getTime().getMinute(),
+                                dateTimeBean.getTime().getSecond()))); // TODO: Faire plus propre ?
+
+
+        ComboBox timeZoneComboBox = new ComboBox();
+        timeZoneComboBox.setStyle("-fx-pref-width: 180;");
+        timeZoneComboBox.setItems(FXCollections.observableArrayList(
+                ZoneId.getAvailableZoneIds()).sorted());
+        timeZoneComboBox.valueProperty().addListener(
+                (p, o, n) -> dateTimeBean.setZone(ZoneId.of(n.toString()))); // TODO: A vérifier / Faire un lien bidirectionnel ?
+        timeZoneComboBox.valueProperty().bind(dateTimeBean.zoneProperty());
+
+
+        // Désactivation des champs lors d'une animation
+        datePicker.disableProperty().bind(timeAnimator.runningProperty());
+        timeTextField.disableProperty().bind(timeAnimator.runningProperty());
+        timeZoneComboBox.disableProperty().bind(timeAnimator.runningProperty());
+
+        HBox observedDateTimeHBox = new HBox(dateLabel, datePicker, timeLabel, timeTextField, timeZoneComboBox);
+        observedDateTimeHBox.setStyle(CONTROL_BAR_HBOX_CHILDREN_STYLE);
+
+        return observedDateTimeHBox;
+    }
 
     /**
      * Méthode privée qui retourne le stream contenu dans le dossier
@@ -190,34 +209,69 @@ public class Main extends Application {
     }
 
     /**
+     * Méthode privée qui créé et retourne le panneau de
+     * l'écoulement du temps.
+     *
+     * @return le panneau de l'écoulement du temps
+     * @throws IOException en cas d'erreur entrée / sortie
+     */
+    private HBox createTimeAnimationHBox() throws IOException {
+        ChoiceBox acceleratorChoiceBox = new ChoiceBox();
+        acceleratorChoiceBox.setItems(FXCollections.observableList(NamedTimeAccelerator.ALL));
+        acceleratorChoiceBox.valueProperty().bind(Bindings.select(timeAnimator.acceleratorProperty(), "name"));// TODO: A vérifier
+        acceleratorChoiceBox.valueProperty().bind(timeAnimator.acceleratorProperty());
+
+        Button resetButton = new Button(UNDO_ICON);
+        // TODO: lier le bouton au bean associé
+        Button playPauseButton = new Button(PLAY_ICON);
+
+        try (InputStream fontStream = resourceStream("/Font Awesome 5 Free-Solid-900.otf")) {
+            Font fontAwesome = Font.loadFont(fontStream, 15);
+            resetButton.setFont(fontAwesome);
+            playPauseButton.setFont(fontAwesome);
+        }
+
+
+        playPauseButton.setOnMousePressed(e -> {
+            if (! timeAnimator.getRunning()) {
+                playPauseButton.setText(PAUSE_ICON);
+                timeAnimator.start();
+            } else {
+                playPauseButton.setText(PLAY_ICON);
+                timeAnimator.stop();
+            }
+        });
+
+        HBox timeAnimationHBox = new HBox(acceleratorChoiceBox, resetButton, playPauseButton);
+        timeAnimationHBox.setStyle("-fx-spacing: inherit;");
+
+        return timeAnimationHBox;
+    }
+
+    /**
      * Méthode privée qui retourne le panneau du ciel.
      *
      * @return le panneau du ciel
+     * @throws IOException en cas d'erreur entrée / sortie
      */
     private Pane createSky() throws IOException {
         Pane skyPane = null;
-        try (InputStream hs = resourceStream("/hygdata_v3.csv")) {
-            StarCatalogue catalogue = new StarCatalogue.Builder()
-                    .loadFrom(hs, HygDatabaseLoader.INSTANCE)
-                    .build();
+        try (InputStream hs1 = resourceStream("/hygdata_v3.csv")) {
+            try (InputStream hs2 = resourceStream("/asterisms.txt")) { // TODO: Eviter ce double bloc try
+                StarCatalogue catalogue = new StarCatalogue.Builder()
+                        .loadFrom(hs1, HygDatabaseLoader.INSTANCE)
+                        .loadFrom(hs2, AsterismLoader.INSTANCE)
+                        .build();
 
-            // TEMPORTAIRE
-            observerLocationBean.setCoordinates(
-                    GeographicCoordinates.ofDeg(6.57, 46.52));
-            viewingParametersBean.setCenter(
-                    HorizontalCoordinates.ofDeg(180.000001, 42));
-            viewingParametersBean.setFieldOfViewDeg(70);
-            dateTimeBean.setZonedDateTime(ZonedDateTime.parse("2020-02-17T20:15:00+01:00"));
-            // FIN TEMPORAIRE
-
-            skyCanvasManager =
-                    new SkyCanvasManager(catalogue,
-                            dateTimeBean, observerLocationBean, viewingParametersBean);
-            Canvas skyCanvas = skyCanvasManager.canvas();
-            skyPane = new Pane(skyCanvas);
-            skyCanvas.widthProperty().bind(skyPane.widthProperty());
-            skyCanvas.heightProperty().bind(skyPane.heightProperty());
-            skyCanvas.requestFocus(); // TODO: Faire cet appel après la méthode show ...
+                skyCanvasManager =
+                        new SkyCanvasManager(catalogue,
+                                dateTimeBean, observerLocationBean, viewingParametersBean);
+                Canvas skyCanvas = skyCanvasManager.canvas();
+                skyPane = new Pane(skyCanvas);
+                skyCanvas.widthProperty().bind(skyPane.widthProperty());
+                skyCanvas.heightProperty().bind(skyPane.heightProperty());
+                skyCanvas.requestFocus(); // TODO: Faire cet appel après la méthode show ...
+            }
         }
         return skyPane;
     }
